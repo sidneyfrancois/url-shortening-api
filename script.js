b89bd08533ac5b0aac8d3fdcc94f5daa6aa79c61
Sidney François <sidney.fvm.dev@outlook.com>
2023-06-21 11:50:14 -0300

    generate result container with result, javascript and style added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btnToggleMenu = document.getElementById("nav-toggle")
 const inputURL = document.getElementById('url-input')
 const btnSubmitURL = document.getElementById('submit-url')
 const errorMessage = document.getElementById('error-label')
+const resultContainer = document.getElementById('result')
 
 function toggleMenu() {
   navbar.classList.toggle("nav-mobile-toggle");
@@ -25,6 +26,24 @@ function removeErrorMessage() {
  errorMessage.classList.add('error-msg-show') 
 }
 
+function createURLResultContainer(originalURL, tinyURL) {
+  let resultBox = document.createElement('div')
+  let originalURLSpan = document.createElement('span')
+  let tinyURLSpan = document.createElement('span')
+  let buttonCopy = document.createElement('button')
+  
+  originalURLSpan.textContent = originalURL 
+  tinyURLSpan.textContent = 'tiny test'
+  buttonCopy.textContent = 'copy'
+  
+  resultBox.appendChild(originalURLSpan)
+  resultBox.appendChild(tinyURLSpan)
+  resultBox.appendChild(buttonCopy)
+  resultBox.className = 'result-box'
+  
+  resultContainer.appendChild(resultBox) 
+}
+
 function validateURL(url) {
   try {
     new URL(url)
@@ -41,6 +60,7 @@ function createTinyLink() {
   } else {
     removeErrorInputStyle()
     removeErrorMessage()
+    createURLResultContainer(inputURL.value, 'testtt')
   }
 }
 
