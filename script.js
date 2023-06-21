547c2211da58041efa800222c91a1457f495fe6b
Sidney François <sidney.fvm.dev@outlook.com>
2023-06-21 10:34:08 -0300

    add simple validation, script and style

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-const navbar = document.getElementById("navbar");
-const btnToggleMenu = document.getElementById("nav-toggle");
+const navbar = document.getElementById("navbar")
+const btnToggleMenu = document.getElementById("nav-toggle")
+
+const inputURL = document.getElementById('url-input')
+const btnSubmitURL = document.getElementById('submit-url')
+const errorMessage = document.getElementById('error-label')
 
 function toggleMenu() {
-  console.log("clikou");
   navbar.classList.toggle("nav-mobile-toggle");
 }
 
+function addErrorInputStyle() {
+  inputURL.classList.add('input-error')
+}
+
+function removeErrorInputStyle() {
+  inputURL.classList.remove('input-error')
+}
+
+function addErrorMessage() {
+ errorMessage.classList.remove('error-msg-show') 
+}
+
+function removeErrorMessage() {
+ errorMessage.classList.add('error-msg-show') 
+}
+
+function validateURL(url) {
+  try {
+    new URL(url)
+    return true
+  } catch (err) {
+    return false
+  }
+}
+
+function createTinyLink() {
+  if (!validateURL(inputURL.value)) {
+    addErrorInputStyle()
+    addErrorMessage()
+  } else {
+    removeErrorInputStyle()
+    removeErrorMessage()
+  }
+}
+
 btnToggleMenu.addEventListener("click", toggleMenu);
+btnSubmitURL.addEventListener('click', createTinyLink)
